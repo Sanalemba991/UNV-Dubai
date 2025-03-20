18a16cdcf46574260536ccbd60bde1dd380f13f2
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2025-03-20 13:11:32 +0530

    all got goods now

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,29 @@
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection } from "astro:content";
 
-const productsCollection = defineCollection({
+const laptopCollection = defineCollection({
   type: "content",
-  schema: ({ image }) => z.object({
-    id: z.number(),
-    title: z.string(),
-    body: z.string(),
-    icon: z.string().optional()
-  }),
+  schema: ({ image }) =>
+    z.object({
+      id: z.number(),
+      title: z.string(),
+      body: z.string(),
+      imageCard: image(),
+    }),
+});
+const networkCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      id: z.number(),
+      title: z.string(),
+      body: z.string(),
+      imageCard: image(),
+      description: z.string().optional(),
+    }),
 });
 
-
-
-
 const ipCamerasCollection = defineCollection({
-  type: 'content',
+  type: "content",
   schema: z.object({
     title: z.string(),
     description: z.string(),
@@ -155,119 +164,124 @@ const accessoriesSchema = z.object({
 
 const blogCollection = defineCollection({
   type: "content",
-  schema: ({ image }) => z.object({
-    id: z.number(),
-    title: z.string(),
-    description: z.string(),
-    contents: z.array(z.string()),
-    author: z.string(),
-    role: z.string().optional(),
-    authorImage: image(),
-    authorImageAlt: z.string(),
-    pubDate: z.date(),
-    cardImage: image(),
-    cardImageAlt: z.string(),
-    readTime: z.number(),
-    tags: z.array(z.string()).optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      id: z.number(),
+      title: z.string(),
+      description: z.string(),
+      contents: z.array(z.string()),
+      author: z.string(),
+      role: z.string().optional(),
+      authorImage: image(),
+      authorImageAlt: z.string(),
+      pubDate: z.date(),
+      cardImage: image(),
+      cardImageAlt: z.string(),
+      readTime: z.number(),
+      tags: z.array(z.string()).optional(),
+    }),
 });
 
 const insightsCollection = defineCollection({
   type: "content",
-  schema: ({ image }) => z.object({
-    id: z.number(),
-    pubDate: z.date(),
-    title: z.string(),
-    description: z.string(),
-    contents: z.array(z.string()),
-    cardImage: image(),
-    cardImageAlt: z.string(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      id: z.number(),
+      pubDate: z.date(),
+      title: z.string(),
+      description: z.string(),
+      contents: z.array(z.string()),
+      cardImage: image(),
+      cardImageAlt: z.string(),
+    }),
 });
 
 const testimonialCollection = defineCollection({
   type: "content",
-  schema: ({ image }) => z.object({
-    id: z.number(),
-    content: z.string(),
-    author: z.string(),
-    role: z.string(),
-    avatarSrc: image(),
-    avatarAlt: z.string(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      id: z.number(),
+      content: z.string(),
+      author: z.string(),
+      role: z.string(),
+      avatarSrc: image(),
+      avatarAlt: z.string(),
+    }),
 });
 
 const featureCollection = defineCollection({
   type: "content",
-  schema: ({ image }) => z.object({
-    id: z.number(),
-    title: z.string(),
-    body: z.string(),
-    icon: z.string().optional()
-  }),
+  schema: ({ image }) =>
+    z.object({
+      id: z.number(),
+      title: z.string(),
+      body: z.string(),
+      icon: z.string().optional(),
+    }),
 });
 
 const teamCollection = defineCollection({
   type: "content",
-  schema: ({ image }) => z.object({
-    draft: z.boolean(),
-    name: z.string(),
-    title: z.string(),
-    avatar: z.object({
-      src: image(),
-      alt: z.string(),
+  schema: ({ image }) =>
+    z.object({
+      draft: z.boolean(),
+      name: z.string(),
+      title: z.string(),
+      avatar: z.object({
+        src: image(),
+        alt: z.string(),
+      }),
+      publishDate: z.string().transform((str) => new Date(str)),
     }),
-    publishDate: z.string().transform(str => new Date(str)),
-  }),
 });
 
 const sliderCollection = defineCollection({
   type: "content",
-  schema: ({ image }) => z.object({
-    id: z.number(),
-    slider: z.string(),
-    sliderAlt: z.string(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      id: z.number(),
+      slider: z.string(),
+      sliderAlt: z.string(),
+    }),
 });
 
 export const collections = {
-  'ip-cameras': ipCamerasCollection,
-  'analog-cameras': defineCollection({
-    type: 'content',
+  "ip-cameras": ipCamerasCollection,
+  "analog-cameras": defineCollection({
+    type: "content",
     schema: analogCamerasSchema,
   }),
-  'ptz-cameras': defineCollection({
-    type: 'content',
+  "ptz-cameras": defineCollection({
+    type: "content",
     schema: ptzCamerasSchema,
   }),
-  'thermal-cameras': defineCollection({
-    type: 'content',
+  "thermal-cameras": defineCollection({
+    type: "content",
     schema: thermalCamerasSchema,
   }),
-  'nvr': defineCollection({
-    type: 'content',
+  nvr: defineCollection({
+    type: "content",
     schema: nvrSchema,
   }),
-  'dvr': defineCollection({
-    type: 'content',
+  dvr: defineCollection({
+    type: "content",
     schema: dvrSchema,
   }),
-  'storage': defineCollection({
-    type: 'content',
+  storage: defineCollection({
+    type: "content",
     schema: storageSchema,
   }),
-  'accessories': defineCollection({
-    type: 'content',
+  accessories: defineCollection({
+    type: "content",
     schema: accessoriesSchema,
   }),
-  
 
-
-  'blog': blogCollection,
-  'insights': insightsCollection,
-  'testimonials': testimonialCollection,
-  'feature': featureCollection,
-  'slider': sliderCollection,
-  'teams': teamCollection,
-  'products': productsCollection
+  blog: blogCollection,
+  insights: insightsCollection,
+  testimonials: testimonialCollection,
+  feature: featureCollection,
+  slider: sliderCollection,
+  teams: teamCollection,
+  laptop: laptopCollection,
+  network: networkCollection,
 };
