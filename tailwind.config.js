83146688fd172410c2afc2018fbe9b53b846f97d
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2025-05-01 13:59:08 +0530

    dropdownbar smooth transation

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
   './node_modules/preline/preline.js',],
+  
   theme: {
     extend: {
       colors: {
