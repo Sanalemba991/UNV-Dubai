740ca2b58dea6e862083a59a29eaf3ad60cac3e5
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2025-12-05 12:40:31 +0530

    perfect

--- dist/chunks/insight-2.8TgN6gFx.js
+++
@@ -1,1 +0,0 @@
-async function getMod(){return import("./insight-2.CUX8YrDb.js")}const collectedLinks=[],collectedStyles=[],collectedScripts=[],defaultMod={__astroPropagation:!0,getMod:getMod,collectedLinks:collectedLinks,collectedStyles:collectedStyles,collectedScripts:collectedScripts};export{defaultMod as default};
